59ca590c09e1853a41c6ebdbf5605eec5eaa5469
mrg <mrg@NetBSD.org>
2024-10-30 21:16:58 +0000

    merge xorg-server 21.1.14.

--- external/mit/xorg-server/include/version-config.h
+++ external/mit/xorg-server/include/version-config.h
@@ -5,13 +5,13 @@
 #define VERSION_CONFIG_H
 
 /* Vendor man version */
-#define VENDOR_MAN_VERSION "Version 21.1.13"
+#define VENDOR_MAN_VERSION "Version 21.1.14"
 
 /* Vendor name */
 #define VENDOR_NAME "The X.Org Foundation"
 
 /* Vendor release */
-#define VENDOR_RELEASE ((10000000) + ((21) * 100000) + ((1) * 1000) + 13)
+#define VENDOR_RELEASE ((10000000) + ((21) * 100000) + ((1) * 1000) + 14)
 
 #endif /* VERSION_CONFIG_H */
 
